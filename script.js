43b5aaf1be1bff5545ce2490d3712103cb63c8a5
bestryful-dev <bestryful.dev@gmail.com>
2026-02-16 15:21:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+// expanding projects
+const panels =document.querySelectorAll('.panel')
+
+console.log(panels)
+panels.forEach((panel)=> {
+    panel.addEventListener('click', ()=>{
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses (){
+    panels.forEach(function(panel){
+        panel.classList.remove('active')
+    })
+}
+
+// scroll animaton
+const boxes = document.querySelectorAll('.card')
+
+window.addEventListener('scroll', checkboxes)
+
+checkboxes()
+
+function checkboxes(){
+    const triggerBottom = window.innerHeight /5 * 4.5
+
+    boxes.forEach(box => {
+        const boxTop =box.getBoundingClientRect().top
+        
+        if (boxTop< triggerBottom){
+            box.classList.add('show')
+        }else{
+            box.classList.remove('show')
+        }
+    })
+}
+
+// nav minimize on scroll
+const minimize = document.getElementById('navbar')
+console.log(minimize)
+window.addEventListener('scroll', fixNav)
+
+
+function fixNav(){
+    if(window.scrollY>minimize.offsetHeight+150){
+        minimize.classList.add('active')
+    }else{
+        minimize.classList.remove('active')
+    }
+
+}
